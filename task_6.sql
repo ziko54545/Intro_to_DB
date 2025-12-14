9e0163806da0c0580cb66849aa6d95f8fca678b9
Zakaria  Hannane <zakariahannane28@gmail.com>
2025-12-14 14:55:43 +0100

    Fix formatting of INSERT statement in task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,3 +3,4 @@ INSERT INTO customers (customer_id, customer_name, email, address) VALUES
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
+
